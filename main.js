f6699fcd8c5bca8782ce5cd99fa71a061a977694
Vinh Nguyễn Khắc <vinh.nguyen123@hcmut.edu.vn>
2025-01-08 00:12:44 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron');
+const { autoUpdater } = require('electron-updater');
+
+// Kiểm tra bản cập nhật
+autoUpdater.checkForUpdatesAndNotify();
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  win.loadURL('https://www.example.com');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
